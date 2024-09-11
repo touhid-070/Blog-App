2fa8bd11ca12d1ef181c92c0f9294eba5186f64f
Touhid Ansari <touhidansari187@gmail.com>
2024-09-11 11:05:47 +0530

    complete create a post page ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,8 @@ import Projects from './pages/Projects'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import PrivateRoute from './components/PrivateRoute'
+import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute'
+import CreatePost from './pages/CreatePost'
 
 const App = () => {
   return (
@@ -19,9 +21,15 @@ const App = () => {
       <Route path='/about' element={<About/>} />
       <Route path='/sign-in' element={<SignIn/>} />
       <Route path='/sign-up' element={<SignUp/>} />
+
       <Route element={<PrivateRoute/>}>
       <Route path='/dashboard' element={<Dashboard/>} />
       </Route>
+
+      <Route element={<OnlyAdminPrivateRoute/>}>
+      <Route path='/create-post' element={<CreatePost/>} />
+      </Route>
+
       <Route path='/projects' element={<Projects/>} />
     </Routes>
     <Footer/>
