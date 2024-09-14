c1bf31c1d59b2eea46f01b60365f846e5313f3af
Touhid Ansari <touhidansari187@gmail.com>
2024-09-14 19:34:59 +0530

    add posts section to the dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import {useLocation} from 'react-router-dom'
 import DashSidebar from '../components/DashSidebar'
 import DashProfile from '../components/DashProfile'
+import DashPost from '../components/DashPost'
 
 
 export default function Dashboard() {
@@ -22,6 +23,9 @@ export default function Dashboard() {
       </div>
       {/**profile */}
       { tab === 'profile' && <DashProfile/>}
+      {/**Posts */}
+      { tab === 'posts' && <DashPost/>}
+
     </div>
   )
 }
