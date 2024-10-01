22b021922cce9f60669b96935e8628b2f19b339c
Touhid Ansari <touhidansari187@gmail.com>
2024-10-01 19:56:27 +0530

    add delete functionality to the comment section

--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -5,7 +5,7 @@ import { useSelector } from 'react-redux';
 import { Button, Textarea } from 'flowbite-react';
 
 
-export default function Comment({ comment, onLike, onEdit }) {
+export default function Comment({ comment, onLike, onEdit, onDelete }) {
     const [user, setUser] = useState({});
     const [isEditing, setIsEditing] = useState(false);
     const [editedContent, setEditedContent] = useState(comment.content);
@@ -35,11 +35,11 @@ export default function Comment({ comment, onLike, onEdit }) {
             const res = await fetch(`/api/comment/editComment/${comment._id}`, {
                 method: 'PUT',
                 headers: {
-                    'Content-Type': 'application/json'
+                    'Content-Type': 'application/json',
                 },
                 body: JSON.stringify({
-                    content: editedContent
-                })
+                    content: editedContent,
+                }),
             });
             if (res.ok) {
                 setIsEditing(false);
@@ -48,8 +48,7 @@ export default function Comment({ comment, onLike, onEdit }) {
         } catch (error) {
             console.log(error.message);
         }
-
-    }
+    };
     return (
         <div className='flex p-4 border-b dark:border-gray-600 text-sm'>
             <div className='flex-shrink-0 mr-3'>
@@ -75,7 +74,7 @@ export default function Comment({ comment, onLike, onEdit }) {
                             value={editedContent}
                             onChange={(e) => setEditedContent(e.target.value)}
                         />
-                        <div className="flex justify-end gap-2 text-xs">
+                        <div className='flex justify-end gap-2 text-xs'>
                             <Button
                                 type='button'
                                 size='sm'
@@ -117,13 +116,22 @@ export default function Comment({ comment, onLike, onEdit }) {
                             </p>
                             {currentUser &&
                                 (currentUser._id === comment.userId || currentUser.isAdmin) && (
-                                    <button
-                                        type='button'
-                                        onClick={handleEdit}
-                                        className='text-gray-400 hover:text-blue-500'
-                                    >
-                                        Edit
-                                    </button>
+                                    <>
+                                        <button
+                                            type='button'
+                                            onClick={handleEdit}
+                                            className='text-gray-400 hover:text-blue-500'
+                                        >
+                                            Edit
+                                        </button>
+                                        <button
+                                            type='button'
+                                            onClick={() => onDelete(comment._id)}
+                                            className='text-gray-400 hover:text-red-500'
+                                        >
+                                            Delete
+                                        </button>
+                                    </>
                                 )}
                         </div>
                     </>
